a64d82e7be6d15a10366edc098bd10e09f65082b
Thiago da Silva Pinto <email.thiagosp@gmail.com>
2020-03-20 06:38:22 -0300

    PHP Composer - Aula 02 - Atividade 08 (Completa)

--- BuscadorDeCurosos/buscador-cursos.php
+++ BuscadorDeCurosos/buscador-cursos.php
@@ -7,7 +7,7 @@ use Alura\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$client = new Client(['verify' => false, 'base_uri' => 'http://www.alura.com.br/']);
 $crawler = new Crawler();
 
 $buscador = new Buscador($client, $crawler);
