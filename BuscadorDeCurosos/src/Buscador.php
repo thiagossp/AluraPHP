c16cf8559e15c3a8ef4837a124c6ffa8e0f58098
Thiago da Silva Pinto <email.thiagosp@gmail.com>
2020-03-22 10:27:17 -0300

    PHP Composer - Aula 04 - Atividade 05 | Configuracao do .gitignore | Verificacao do padrao PSR12

--- BuscadorDeCurosos/src/Buscador.php
+++ BuscadorDeCurosos/src/Buscador.php
@@ -5,16 +5,19 @@ namespace Alura\BuscadorDeCursos;
 use GuzzleHttp\ClientInterface;
 use Symfony\Component\DomCrawler\Crawler;
 
-class Buscador {
+class Buscador
+{
     private $httpClient;
     private $crawler;
     
-    function __construct(ClientInterface $httpClient, Crawler $crawler) {
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
         $this->httpClient = $httpClient;
         $this->crawler = $crawler;
     }
 
-    public function buscar(string $url): array {
+    public function buscar(string $url): array
+    {
         $resposta = $this->httpClient->request('GET', $url);
 
         $html = $resposta->getBody();
@@ -29,6 +32,4 @@ class Buscador {
 
         return $cursos;
     }
-    
-    
 }
--- BuscadorDeCurosos/vendor/composer/autoload_psr4.php
+++ BuscadorDeCurosos/vendor/composer/autoload_psr4.php
@@ -6,13 +6,18 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'phpDocumentor\\Reflection\\' => array($vendorDir . '/phpdocumentor/reflection-common/src', $vendorDir . '/phpdocumentor/reflection-docblock/src', $vendorDir . '/phpdocumentor/type-resolver/src'),
+    'Webmozart\\Assert\\' => array($vendorDir . '/webmozart/assert/src'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
     'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
     'Symfony\\Component\\DomCrawler\\' => array($vendorDir . '/symfony/dom-crawler'),
     'Symfony\\Component\\CssSelector\\' => array($vendorDir . '/symfony/css-selector'),
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
+    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src/Prophecy'),
     'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
     'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
     'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
+    'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
+    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
     'Alura\\BuscadorDeCursos\\' => array($baseDir . '/src'),
 );
